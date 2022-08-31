7f1cbf85278cd732b1e32e7bfedc59f32b6ef819
Andrew-Carlson <acarlson2398@gmail.com>
2022-08-30 21:08:19 -0400

    Filtering function for table data created

--- static/js/app.js
+++ static/js/app.js
@@ -30,3 +30,37 @@ function buildTable(data) {
         );    
     });
 };
+
+// create function to filter table data
+function handleClick() {
+
+    // D3.js selects first element that matches selector string (HTML id): "datetime". 
+    // The selector string is the HTML item we're telling D3.js to look for.
+    let date = d3.select("#datetime").property("value"); // .property("value"): grabs selected info and puts in "date" variable
+
+    // By setting the filteredData variable to our raw data, we're basically using it as a blank slate.
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the
+    // data using that date with if statement
+    // Pseudocode:
+    // if (a date is entered) {
+        // Filter the default data to show only the date entered
+    if (date) {
+
+        // Show only the rows where the date is equal to the date filter we created above
+        filteredData = filteredData.filter(row => row.datetime === date) // === : strict equality, == : loose equality
+    }
+
+    // if no date was entered, the filteredData table will just be the original tableData
+
+    // call the handleClick() function to update the table with filtered data
+    buildTable(filteredData);
+};
+
+// link code directly to filter button.
+// .on("click", handleClick);, tells D3 to execute handleClick() function when the button with id=filter-btn is clicked.
+d3.selectALL("#filter-btn").on("click", handleClick); // 'filter-btn' is HTML tag/selector string
+
+// Build the table when the page loads
+buildTable(tableData)
